9f745dbd6e1c96a2d483d2c57c7f315f3ca21cae
Ashleigh Crosby <ashleigh@pocketworks.co.uk>
2018-01-11 13:48:09 +0000

    Resize JS on window change

--- ios/Classes/WebviewController.m
+++ ios/Classes/WebviewController.m
@@ -13,7 +13,8 @@
 - (void)viewWillTransitionToSize:(CGSize)size withTransitionCoordinator:(id<UIViewControllerTransitionCoordinator>)coordinator {
     [super viewWillTransitionToSize:size withTransitionCoordinator:coordinator];
     if (self.webview) {
-        self.webview.frame = CGRectMake(0.0, 0.0, size.width, size.height);
+        self.webview.frame = CGRectMake(self.webview.bounds.origin.x, self.webview.bounds.origin.y, size.width, size.height);
+        [self.webview stringByEvaluatingJavaScriptFromString:[NSString stringWithFormat:@"window.resizeTo(%.2f,%.2f)", size.width, size.height]];
     }
 }
 
